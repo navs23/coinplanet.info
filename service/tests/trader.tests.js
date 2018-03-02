e4c940b3a75d661f387f77af6a52baf6f30c471e
Sharma <Naveen.Sharma@rabobankdev.com>
2018-03-02 15:40:13 +0000

    poloniex support

--- service/tests/trader.tests.js
+++ service/tests/trader.tests.js
@@ -1,12 +1,15 @@
 var trader=require('../trader')
 var _=require("underscore");
 var moment=require('moment');
-
+const APIKEY = 'YHSE1T8Q-Z55W3H0B-CO3INAPG-L84IKK89';
+const SECRET = '9dfc9c57a101f77ea1ca96487b78d804c656bb8c2a0e50c3fbcfea53ee4dc8b3fa4fbf09a48cb933a724cceeb88ce156da0f17e8ad179aaade071a0b3687b1a6';
 trader.returnTradeHistory(
 {
     currencyPair: 'all',
-    start: new Date( moment().subtract(3, 'months')).getTime() / 1000,
-     end: new Date(Date.now).getTime() / 1000
+    start: 1517582570.092,//new Date( moment().subtract(3, 'months')).getTime() / 1000,
+     end: 1520001770.093,//new Date(Date.now).getTime() / 1000,
+    key:APIKEY,
+    secret:SECRET
   }
 )
   .then(msg => console.log(
--- website/routes/exchange.js
+++ website/routes/exchange.js
@@ -4,17 +4,18 @@
     var trader = require("./../../service/trader");
     var _=require("underscore");
 
-
+    var moment=require('moment');
     
     exchnage.init= function(router){
         
         /* GET home page. */
 
        router.get('/trade/', function(req, res, next) {
+           /*
            let filter={
             currencyPair: 'all',
             start: new Date('2017-01-01 00:00:00').getTime() / 1000,
-             end: new Date('2018-02-28 05:43:30').getTime() / 1000
+            end: new Date('2018-02-28 05:43:30').getTime() / 1000
           }
         trader.returnTradeHistory(filter)
               .then(response => {
@@ -31,24 +32,26 @@
                     res.render('trades', {title: 'Trade Manager',trades:[],error:err,currencyPairs:[] });
                 })
              
-
+                */
      
-       
+               res.render('trades', {title: 'Trade Manager',error:null ,currencyPairs:[],trades:[]});
       
     });    
     router.post('/api/trade/', function(req, res, next) {
-    //     let filter={
-    //      currencyPair: 'all',
-    //      start: new Date('2017-01-01 00:00:00').getTime() / 1000,
-    //       end: new Date('2018-02-28 05:43:30').getTime() / 1000
-    //    }
-
-     trader.returnTradeHistory(filter)
+        var param=req.body;
+        if(param.history==0) param.history=60;
+        param.start=new Date( moment().subtract(param.history || 3, 'months')).getTime() / 1000;
+        param.end=new Date(Date.now).getTime() / 1000
+        
+     trader.returnTradeHistory(param)
            .then(response => {
              var tradesJson=JSON.parse(response.body);
              var currencyPairs=_.keys(tradesJson); 
-            // console.log(currencyPairs);
-             res.send({title: 'Trade history',error:null ,currencyPairs:currencyPairs,trades:tradesJson});
+              if (!tradesJson.error)           
+                res.send({title: 'Trade history',error:null ,currencyPairs:currencyPairs,trades:tradesJson});
+              else {
+                res.send({title: 'Trade history',error:tradesJson.error ,currencyPairs:currencyPairs,trades:[]});  
+              }
            }
          )
          .catch(
