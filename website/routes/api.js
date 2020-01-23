69081576d16e89c4054345bcdafb3690a7381528
Glitch (navs23-coinplanet-info) <none>
2020-01-23 11:03:02 +0000

    🍎🐺 Checkpoint ./website/routes/api.js:10476208/63 ./service/currentPrice.js:10476208/2883

--- website/routes/api.js
+++ website/routes/api.js
@@ -5,7 +5,9 @@ var service = require("./../../service/currentPrice");
 api.init= function(router){
 	
 	console.log("initialising %s route","api");
-	router.get('/_api/fiatrates/:/ccy', function(req, res) {
+	router.get('/_api/fiatrates/:CCY?', function(req, res) {
+    let {CCY} = req.params;
+    
     let ratesPromise = service.getFiatExchangeRates();
     ratesPromise.then(rates=>{
         res.json(rates);    
