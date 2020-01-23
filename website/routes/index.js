ff49e6cf68b16f9979d37a269703cc4df1116f4a
Glitch (navs23-coinplanet-info) <none>
2020-01-23 10:27:02 +0000

    ⛽️🏁 Checkpoint ./website/routes/index.js:10476208/104

--- website/routes/index.js
+++ website/routes/index.js
@@ -2,10 +2,11 @@
 
 var home = require("./home.js");
 var exchange = require("./exchange.js");
-
+var api = require("./api.js");
 main.init =function(router){
         home.init(router);
         exchange.init(router);
+        api.init(router);
     
 }
 
