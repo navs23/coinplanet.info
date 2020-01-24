c6b84478d7680ad574fead6f5f61ea16afcfe1fc
Glitch (navs23-coinplanet-info) <none>
2020-01-24 16:38:38 +0000

    🍼✌️ Checkpoint ./website/routes/home.js:10476208/1788 ./website/helper/news.js:10476208/996 ./service/news.js:10476208/10385

---
+++ service/news.js
@@ -0,0 +1,28 @@
+//
+
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NES_API_KEY);
+
+  let getNews=function(cb){
+
+	newsapi.v2.everything({
+		q: 'bitcoin',
+		//sources: 'bbc-news,the-verge',
+		//domains: 'bbc.co.uk, techcrunch.com',
+		//from: '2017-12-01',
+		//to: '2017-12-12',
+		language: 'en',
+		sortBy: 'publishedAt',
+		page: 5
+	  }).then(response => {
+		
+		return cb(null,response.articles);
+	  })
+	 .catch(function(err){
+		 console.log('error');
+		return cb(err,[]);
+	 });
+  }
+  
+export {getNews}
