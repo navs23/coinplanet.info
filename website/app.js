ccb1b5d372bd7dec724414dc7c7fd76c9fe7db33
Glitch (navs23-coinplanet-info) <none>
2020-01-24 08:46:41 +0000

    🚀🐌 Checkpoint ./website/app.js:10476208/95

--- website/app.js
+++ website/app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const listEndpoints = require('express-list-endpoints')
 var cache={};
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -46,6 +47,8 @@ if (!process.env.DISABLE_XORIGIN) {
  
 
 index.init(app);
+
+let entponits = listEndpoints(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
